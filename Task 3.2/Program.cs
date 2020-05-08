77a5b52ad40622f908f04ae6707904bd6868b429
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-08 13:27:15 +0300

    Добавьте файлы проекта.

---
+++ Task 3.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3._2
+{
+    class Program
+    {
+        public static double  f(double x)
+        {
+            double s;
+            if (Math.Abs(x) < 2)
+            {
+                s = Math.Sqrt(5 * x * x + 5);
+            }
+            else if (Math.Abs(x) >= 2 && Math.Abs(x) < 10)
+            {
+                s = Math.Abs(x) / Math.Sqrt(5 * x * x + 5);
+            }
+            else
+                s = 0;
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            double a, b, h;
+            Console.WriteLine("Введите а: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите шаг h: ");
+            h = Convert.ToDouble(Console.ReadLine());
+            for(double i = a; i <= b; i = i + h)
+            {
+                Console.WriteLine(f(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
